a7c6f7176b4bc81036ca150dbd7d63368fdad96f
우창희 <w1373@hanmail.net>
2025-09-10 00:44:11 +0900

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/cloude-code-majayoung-nextjs',
   trailingSlash: true,
   images: {
     unoptimized: true
